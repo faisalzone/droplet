d179374ed81dda4fb67eee528b2425e93e5984f2
faisalzone <faisalzone@gmail.com>
2020-08-23 15:36:11 +1000

    final commit

--- game/urls.py
+++ game/urls.py
@@ -5,7 +5,90 @@ app_name = 'game'
 
 urlpatterns = [
     path('start/', views.start_game, name='start_game'),
-    path('start/day-1/task-1/', views.day_one_task_one, name='day_one_task_one'),
-    path('start/day-1/task-1/yes/', views.day_one_task_one_yes, name='day_one_task_one_yes'),
-    path('start/day-1/task-2/', views.day_one_task_two, name='day_one_task_two'),
+    # Day one tasks
+    path('day-1/task-1/', views.day_one_task_one, name='day_one_task_one'),
+    path('day-1/task-1/yes/', views.day_one_task_one_yes, name='day_one_task_one_yes'),
+    path('day-1/task-2/', views.day_one_task_two, name='day_one_task_two'),
+    path('day-1/task-2/yes/', views.day_one_task_two_yes, name='day_one_task_two_yes'),
+    path('day-1/task-3/', views.day_one_task_three, name='day_one_task_three'),
+    path('day-1/task-3/yes/', views.day_one_task_three_yes, name='day_one_task_three_yes'),
+    path('day-1/task-4/', views.day_one_task_four, name='day_one_task_four'),
+    path('day-1/task-4/yes/', views.day_one_task_four_yes, name='day_one_task_four_yes'),
+    path('day-1/task-5/', views.day_one_task_five, name='day_one_task_five'),
+    path('day-1/task-5/yes/', views.day_one_task_five_yes, name='day_one_task_five_yes'),
+    path('day-1/task-6/', views.day_one_task_six, name='day_one_task_six'),
+    path('day-1/task-6/yes/', views.day_one_task_six_yes, name='day_one_task_six_yes'),
+    path('day-1/task-7/', views.day_one_task_seven, name='day_one_task_seven'),
+    path('day-1/task-7/yes/', views.day_one_task_seven_yes, name='day_one_task_seven_yes'),
+
+    # Day two tasks
+    path('day-2/task-1/', views.day_two_task_one, name='day_two_task_one'),
+    path('day-2/task-1/yes/', views.day_two_task_one_yes, name='day_two_task_one_yes'),
+    path('day-2/task-2/', views.day_two_task_two, name='day_two_task_two'),
+    path('day-2/task-2/yes/', views.day_two_task_two_yes, name='day_two_task_two_yes'),
+    path('day-2/task-3/', views.day_two_task_three, name='day_two_task_three'),
+    path('day-2/task-3/yes/', views.day_two_task_three_yes, name='day_two_task_three_yes'),
+    path('day-2/task-4/', views.day_two_task_four, name='day_two_task_four'),
+    path('day-2/task-4/yes/', views.day_two_task_four_yes, name='day_two_task_four_yes'),
+    path('day-2/task-5/', views.day_two_task_five, name='day_two_task_five'),
+    path('day-2/task-5/yes/', views.day_two_task_five_yes, name='day_two_task_five_yes'),
+    path('day-2/task-6/', views.day_two_task_six, name='day_two_task_six'),
+    path('day-2/task-6/yes/', views.day_two_task_six_yes, name='day_two_task_six_yes'),
+    path('day-2/task-7/', views.day_two_task_seven, name='day_two_task_seven'),
+    path('day-2/task-7/yes/', views.day_two_task_seven_yes, name='day_two_task_seven_yes'),
+
+    # # Day three tasks
+    # path('day-3/task-1/', views.day_three_task_one, name='day_three_task_one'),
+    # path('day-3/task-1/yes/', views.day_three_task_one_yes, name='day_three_task_one_yes'),
+    # path('day-3/task-2/', views.day_three_task_two, name='day_three_task_two'),
+    # path('day-3/task-2/yes/', views.day_three_task_two_yes, name='day_three_task_two_yes'),
+    # path('day-3/task-3/', views.day_three_task_three, name='day_three_task_three'),
+    # path('day-3/task-3/yes/', views.day_three_task_three_yes, name='day_three_task_three_yes'),
+    # path('day-3/task-4/', views.day_three_task_four, name='day_three_task_four'),
+    # path('day-3/task-4/yes/', views.day_three_task_four_yes, name='day_three_task_four_yes'),
+    # path('day-3/task-5/', views.day_three_task_five, name='day_three_task_five'),
+    # path('day-3/task-5/yes/', views.day_three_task_five_yes, name='day_three_task_five_yes'),
+    # path('day-3/task-6/', views.day_three_task_six, name='day_three_task_six'),
+    # path('day-3/task-6/yes/', views.day_three_task_six_yes, name='day_three_task_six_yes'),
+    # path('day-3/task-7/', views.day_three_task_seven, name='day_three_task_seven'),
+    # path('day-3/task-7/yes/', views.day_three_task_seven_yes, name='day_three_task_seven_yes'),
+    #
+    # # Day four tasks
+    # path('day-4/task-1/', views.day_four_task_one, name='day_four_task_one'),
+    # path('day-4/task-1/yes/', views.day_four_task_one_yes, name='day_four_task_one_yes'),
+    # path('day-4/task-2/', views.day_four_task_two, name='day_four_task_two'),
+    # path('day-4/task-2/yes/', views.day_four_task_two_yes, name='day_four_task_two_yes'),
+    # path('day-4/task-3/', views.day_four_task_three, name='day_four_task_three'),
+    # path('day-4/task-3/yes/', views.day_four_task_three_yes, name='day_four_task_three_yes'),
+    # path('day-4/task-4/', views.day_four_task_four, name='day_four_task_four'),
+    # path('day-4/task-4/yes/', views.day_four_task_four_yes, name='day_four_task_four_yes'),
+    # path('day-4/task-5/', views.day_four_task_five, name='day_four_task_five'),
+    # path('day-4/task-5/yes/', views.day_four_task_five_yes, name='day_four_task_five_yes'),
+    # path('day-4/task-6/', views.day_four_task_six, name='day_four_task_six'),
+    # path('day-4/task-6/yes/', views.day_four_task_six_yes, name='day_four_task_six_yes'),
+    # path('day-4/task-7/', views.day_four_task_seven, name='day_four_task_seven'),
+    # path('day-4/task-7/yes/', views.day_four_task_seven_yes, name='day_four_task_seven_yes'),
+    #
+    # # Day five tasks
+    # path('day-5/task-1/', views.day_five_task_one, name='day_five_task_one'),
+    # path('day-5/task-1/yes/', views.day_five_task_one_yes, name='day_five_task_one_yes'),
+    # path('day-5/task-2/', views.day_five_task_two, name='day_five_task_two'),
+    # path('day-5/task-2/yes/', views.day_five_task_two_yes, name='day_five_task_two_yes'),
+    # path('day-5/task-3/', views.day_five_task_three, name='day_five_task_three'),
+    # path('day-5/task-3/yes/', views.day_five_task_three_yes, name='day_five_task_three_yes'),
+    # path('day-5/task-4/', views.day_four_five_four, name='day_five_task_four'),
+    # path('day-5/task-4/yes/', views.day_five_task_four_yes, name='day_five_task_four_yes'),
+    # path('day-5/task-5/', views.day_five_task_five, name='day_five_task_five'),
+    # path('day-5/task-5/yes/', views.day_five_task_five_yes, name='day_five_task_five_yes'),
+    # path('day-5/task-6/', views.day_five_task_six, name='day_five_task_six'),
+    # path('day-5/task-6/yes/', views.day_five_task_six_yes, name='day_five_task_six_yes'),
+    # path('day-5/task-7/', views.day_five_task_seven, name='day_five_task_seven'),
+    # path('day-5/task-7/yes/', views.day_five_task_seven_yes, name='day_five_task_seven_yes'),
+    #
+    # # Day five tasks
+    # path('day-6/task-1/', views.day_six_task_one, name='day_six_task_one'),
+    #
+    # # Day five tasks
+    # path('day-7/task-1/', views.day_seven_task_one, name='day_seven_task_one'),
+
 ]
--- game/views.py
+++ game/views.py
@@ -8,6 +8,9 @@ def start_game(request):
     return render(request, 'game/start.html')
 
 
+# day_one_tasks
+
+
 def day_one_task_one(request):
     task1 = Task.objects.get(seq=1)
     task1.completed_day_one = False
@@ -111,51 +114,476 @@ def day_one_task_two(request):
     return render(request, 'game/day-one/task-2.html', context)
 
 
+def day_one_task_two_yes(request):
+    task = Task.objects.get(seq=2)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_one = True  # change day_(num)
+    task.save()
+    return redirect('game:day_one_task_three')  # change day_(num)_task_(next)
+
+
 def day_one_task_three(request):
-    day = Day.objects.get(number=1)
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=1)  # change day(num)
+    task = Task.objects.get(seq=3)  # change task(num)
     tasks = Task.objects.all()
     context = {
         'day': day,
+        'task': task,
         'tasks': tasks,
+        'remaining_water': remaining_water,
     }
-    return render(request, 'game/day-one/task-1.html', context)
+    return render(request, 'game/day-one/task-3.html', context)  # change day-(num)/task-(num)
+
+
+def day_one_task_three_yes(request):
+    task = Task.objects.get(seq=3)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_one = True  # change day_(num)
+    task.save()
+    return redirect('game:day_one_task_four')  # change day_(num)_task_(next)
 
 
 def day_one_task_four(request):
-    day = Day.objects.get(number=1)
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=1)  # change day(num)
+    task = Task.objects.get(seq=4)  # change task(num)
     tasks = Task.objects.all()
     context = {
         'day': day,
+        'task': task,
         'tasks': tasks,
+        'remaining_water': remaining_water,
     }
-    return render(request, 'game/day-one/task-1.html', context)
+    return render(request, 'game/day-one/task-4.html', context)  # change day-(num)/task-(num)
+
+
+def day_one_task_four_yes(request):  # change task_(num)
+    task = Task.objects.get(seq=4)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_one = True  # change day_(num)
+    task.save()
+    return redirect('game:day_one_task_five')  # change day_(num)_task_(next)
 
 
 def day_one_task_five(request):
-    day = Day.objects.get(number=1)
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=1)  # change day(num)
+    task = Task.objects.get(seq=5)  # change task(num)
     tasks = Task.objects.all()
     context = {
         'day': day,
+        'task': task,
         'tasks': tasks,
+        'remaining_water': remaining_water,
     }
-    return render(request, 'game/day-one/task-1.html', context)
+    return render(request, 'game/day-one/task-5.html', context)  # change day-(num)/task-(num)
+
+
+def day_one_task_five_yes(request):  # change task_(num)
+    task = Task.objects.get(seq=5)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_one = True  # change day_(num)
+    task.save()
+    return redirect('game:day_one_task_six')  # change day_(num)_task_(next)
 
 
 def day_one_task_six(request):
-    day = Day.objects.get(number=1)
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=1)  # change day(num)
+    task = Task.objects.get(seq=6)  # change task(num)
     tasks = Task.objects.all()
     context = {
         'day': day,
+        'task': task,
         'tasks': tasks,
+        'remaining_water': remaining_water,
     }
-    return render(request, 'game/day-one/task-1.html', context)
+    return render(request, 'game/day-one/task-6.html', context)  # change day-(num)/task-(num)
 
 
-def day_one_task_seven(request):
-    day = Day.objects.get(number=1)
+def day_one_task_six_yes(request):  # change task_(num)
+    task = Task.objects.get(seq=6)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_one = True  # change day_(num)
+    task.save()
+    return redirect('game:day_one_task_seven')  # change day_(num)_task_(next)
+
+
+def day_one_task_seven(request):  # change day_(num)_task_(num)
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=1)  # change day(num)
+    task = Task.objects.get(seq=7)  # change task(num)
     tasks = Task.objects.all()
     context = {
         'day': day,
+        'task': task,
         'tasks': tasks,
+        'remaining_water': remaining_water,
     }
-    return render(request, 'game/day-one/task-1.html', context)
+    return render(request, 'game/day-one/task-7.html', context)  # change day-(num)/task-(num)
+
+
+def day_one_task_seven_yes(request):  # change task_(num)
+    task = Task.objects.get(seq=7)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_one = True  # change day_(num)
+    task.save()
+    return redirect('game:day_two_task_one')  # change day_(num)_task_(next)
+
+
+# day two tasks
+
+
+def day_two_task_one(request):  # change day_(num)_task_(num)
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=2)  # change day(num)
+    task = Task.objects.get(seq=1)  # change task(num)
+    tasks = Task.objects.all()
+    context = {
+        'day': day,
+        'task': task,
+        'tasks': tasks,
+        'remaining_water': remaining_water,
+    }
+    return render(request, 'game/day-two/task-1.html', context)  # change day-(num)/task-(num)
+
+
+def day_two_task_one_yes(request):  # change day_(num)_task_(num)
+    task = Task.objects.get(seq=1)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_two = True  # change day_(num)
+    task.save()
+    return redirect('game:day_two_task_two')  # change day_(num)_task_(next)
+
+
+
+def day_two_task_two(request):
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=2)
+    task = Task.objects.get(seq=2)
+    tasks = Task.objects.all()
+    context = {
+        'day': day,
+        'task': task,
+        'tasks': tasks,
+        'remaining_water': remaining_water,
+    }
+    return render(request, 'game/day-two/task-2.html', context)
+
+
+def day_two_task_two_yes(request):
+    task = Task.objects.get(seq=2)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_two = True  # change day_(num)
+    task.save()
+    return redirect('game:day_two_task_three')  # change day_(num)_task_(next)
+
+
+def day_two_task_three(request):
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=2)  # change day(num)
+    task = Task.objects.get(seq=3)  # change task(num)
+    tasks = Task.objects.all()
+    context = {
+        'day': day,
+        'task': task,
+        'tasks': tasks,
+        'remaining_water': remaining_water,
+    }
+    return render(request, 'game/day-two/task-3.html', context)  # change day-(num)/task-(num)
+
+
+def day_two_task_three_yes(request):
+    task = Task.objects.get(seq=3)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_two = True  # change day_(num)
+    task.save()
+    return redirect('game:day_two_task_four')  # change day_(num)_task_(next)
+
+
+def day_two_task_four(request):
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=2)  # change day(num)
+    task = Task.objects.get(seq=4)  # change task(num)
+    tasks = Task.objects.all()
+    context = {
+        'day': day,
+        'task': task,
+        'tasks': tasks,
+        'remaining_water': remaining_water,
+    }
+    return render(request, 'game/day-two/task-4.html', context)  # change day-(num)/task-(num)
+
+
+def day_two_task_four_yes(request):  # change task_(num)
+    task = Task.objects.get(seq=4)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_two = True  # change day_(num)
+    task.save()
+    return redirect('game:day_two_task_five')  # change day_(num)_task_(next)
+
+
+def day_two_task_five(request):
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=2)  # change day(num)
+    task = Task.objects.get(seq=5)  # change task(num)
+    tasks = Task.objects.all()
+    context = {
+        'day': day,
+        'task': task,
+        'tasks': tasks,
+        'remaining_water': remaining_water,
+    }
+    return render(request, 'game/day-two/task-5.html', context)  # change day-(num)/task-(num)
+
+
+def day_two_task_five_yes(request):  # change task_(num)
+    task = Task.objects.get(seq=5)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_two = True  # change day_(num)
+    task.save()
+    return redirect('game:day_two_task_six')  # change day_(num)_task_(next)
+
+
+def day_two_task_six(request):
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=2)  # change day(num)
+    task = Task.objects.get(seq=6)  # change task(num)
+    tasks = Task.objects.all()
+    context = {
+        'day': day,
+        'task': task,
+        'tasks': tasks,
+        'remaining_water': remaining_water,
+    }
+    return render(request, 'game/day-two/task-6.html', context)  # change day-(num)/task-(num)
+
+
+def day_two_task_six_yes(request):  # change task_(num)
+    task = Task.objects.get(seq=6)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_two = True  # change day_(num)
+    task.save()
+    return redirect('game:day_two_task_seven')  # change day_(num)_task_(next)
+
+
+def day_two_task_seven(request):  # change day_(num)_task_(num)
+    remaining_water = request.session.get('remaining_water')
+    day = Day.objects.get(number=2)  # change day(num)
+    task = Task.objects.get(seq=7)  # change task(num)
+    tasks = Task.objects.all()
+    context = {
+        'day': day,
+        'task': task,
+        'tasks': tasks,
+        'remaining_water': remaining_water,
+    }
+    return render(request, 'game/day-two/task-7.html', context)  # change day-(num)/task-(num)
+
+
+def day_two_task_seven_yes(request):  # change task_(num)
+    task = Task.objects.get(seq=7)  # change for task_(num)
+    remaining_water = request.session.get('remaining_water')
+    new_remaining_water = int(remaining_water) - int(task.water_qty)
+    request.session['remaining_water'] = new_remaining_water
+    task.completed_day_two = True  # change day_(num)
+    task.save()
+    return redirect('game:day_three_task_one')  # change day_(num)_task_(next)
+
+
+# day 3
+#
+#
+# def day_three_task_one(request):  # change day_(num)_task_(num)
+#     remaining_water = request.session.get('remaining_water')
+#     day = Day.objects.get(number=3)  # change day(num)
+#     task = Task.objects.get(seq=1)  # change task(num)
+#     tasks = Task.objects.all()
+#     context = {
+#         'day': day,
+#         'task': task,
+#         'tasks': tasks,
+#         'remaining_water': remaining_water,
+#     }
+#     return render(request, 'game/day-three/task-1.html', context)  # change day-(num)/task-(num)
+#
+#
+# def day_three_task_one_yes(request):  # change day_(num)_task_(num)
+#     task = Task.objects.get(seq=1)  # change for task_(num)
+#     remaining_water = request.session.get('remaining_water')
+#     new_remaining_water = int(remaining_water) - int(task.water_qty)
+#     request.session['remaining_water'] = new_remaining_water
+#     task.completed_day_three = True  # change day_(num)
+#     task.save()
+#     return redirect('game:day_three_task_two')  # change day_(num)_task_(next)
+#
+#
+#
+# def day_three_task_two(request):
+#     remaining_water = request.session.get('remaining_water')
+#     day = Day.objects.get(number=3)
+#     task = Task.objects.get(seq=2)
+#     tasks = Task.objects.all()
+#     context = {
+#         'day': day,
+#         'task': task,
+#         'tasks': tasks,
+#         'remaining_water': remaining_water,
+#     }
+#     return render(request, 'game/day-three/task-2.html', context)
+#
+#
+# def day_three_task_two_yes(request):
+#     task = Task.objects.get(seq=2)  # change for task_(num)
+#     remaining_water = request.session.get('remaining_water')
+#     new_remaining_water = int(remaining_water) - int(task.water_qty)
+#     request.session['remaining_water'] = new_remaining_water
+#     task.completed_day_three = True  # change day_(num)
+#     task.save()
+#     return redirect('game:day_three_task_three')  # change day_(num)_task_(next)
+#
+#
+# def day_three_task_three(request):
+#     remaining_water = request.session.get('remaining_water')
+#     day = Day.objects.get(number=3)  # change day(num)
+#     task = Task.objects.get(seq=3)  # change task(num)
+#     tasks = Task.objects.all()
+#     context = {
+#         'day': day,
+#         'task': task,
+#         'tasks': tasks,
+#         'remaining_water': remaining_water,
+#     }
+#     return render(request, 'game/day-three/task-3.html', context)  # change day-(num)/task-(num)
+#
+#
+# def day_three_task_three_yes(request):
+#     task = Task.objects.get(seq=3)  # change for task_(num)
+#     remaining_water = request.session.get('remaining_water')
+#     new_remaining_water = int(remaining_water) - int(task.water_qty)
+#     request.session['remaining_water'] = new_remaining_water
+#     task.completed_day_three = True  # change day_(num)
+#     task.save()
+#     return redirect('game:day_three_task_four')  # change day_(num)_task_(next)
+#
+#
+# def day_three_task_four(request):
+#     remaining_water = request.session.get('remaining_water')
+#     day = Day.objects.get(number=3)  # change day(num)
+#     task = Task.objects.get(seq=4)  # change task(num)
+#     tasks = Task.objects.all()
+#     context = {
+#         'day': day,
+#         'task': task,
+#         'tasks': tasks,
+#         'remaining_water': remaining_water,
+#     }
+#     return render(request, 'game/day-three/task-4.html', context)  # change day-(num)/task-(num)
+#
+#
+# def day_three_task_four_yes(request):  # change task_(num)
+#     task = Task.objects.get(seq=4)  # change for task_(num)
+#     remaining_water = request.session.get('remaining_water')
+#     new_remaining_water = int(remaining_water) - int(task.water_qty)
+#     request.session['remaining_water'] = new_remaining_water
+#     task.completed_day_three = True  # change day_(num)
+#     task.save()
+#     return redirect('game:day_three_task_five')  # change day_(num)_task_(next)
+#
+#
+# def day_three_task_five(request):
+#     remaining_water = request.session.get('remaining_water')
+#     day = Day.objects.get(number=3)  # change day(num)
+#     task = Task.objects.get(seq=5)  # change task(num)
+#     tasks = Task.objects.all()
+#     context = {
+#         'day': day,
+#         'task': task,
+#         'tasks': tasks,
+#         'remaining_water': remaining_water,
+#     }
+#     return render(request, 'game/day-three/task-5.html', context)  # change day-(num)/task-(num)
+#
+#
+# def day_three_task_five_yes(request):  # change task_(num)
+#     task = Task.objects.get(seq=5)  # change for task_(num)
+#     remaining_water = request.session.get('remaining_water')
+#     new_remaining_water = int(remaining_water) - int(task.water_qty)
+#     request.session['remaining_water'] = new_remaining_water
+#     task.completed_day_three = True  # change day_(num)
+#     task.save()
+#     return redirect('game:day_three_task_six')  # change day_(num)_task_(next)
+#
+#
+# def day_three_task_six(request):
+#     remaining_water = request.session.get('remaining_water')
+#     day = Day.objects.get(number=3)  # change day(num)
+#     task = Task.objects.get(seq=6)  # change task(num)
+#     tasks = Task.objects.all()
+#     context = {
+#         'day': day,
+#         'task': task,
+#         'tasks': tasks,
+#         'remaining_water': remaining_water,
+#     }
+#     return render(request, 'game/day-three/task-6.html', context)  # change day-(num)/task-(num)
+#
+#
+# def day_three_task_six_yes(request):  # change task_(num)
+#     task = Task.objects.get(seq=6)  # change for task_(num)
+#     remaining_water = request.session.get('remaining_water')
+#     new_remaining_water = int(remaining_water) - int(task.water_qty)
+#     request.session['remaining_water'] = new_remaining_water
+#     task.completed_day_three = True  # change day_(num)
+#     task.save()
+#     return redirect('game:day_three_task_seven')  # change day_(num)_task_(next)
+#
+#
+# def day_three_task_seven(request):  # change day_(num)_task_(num)
+#     remaining_water = request.session.get('remaining_water')
+#     day = Day.objects.get(number=3)  # change day(num)
+#     task = Task.objects.get(seq=7)  # change task(num)
+#     tasks = Task.objects.all()
+#     context = {
+#         'day': day,
+#         'task': task,
+#         'tasks': tasks,
+#         'remaining_water': remaining_water,
+#     }
+#     return render(request, 'game/day-three/task-7.html', context)  # change day-(num)/task-(num)
+#
+#
+# def day_three_task_seven_yes(request):  # change task_(num)
+#     task = Task.objects.get(seq=7)  # change for task_(num)
+#     remaining_water = request.session.get('remaining_water')
+#     new_remaining_water = int(remaining_water) - int(task.water_qty)
+#     request.session['remaining_water'] = new_remaining_water
+#     task.completed_day_three = True  # change day_(num)
+#     task.save()
+#     return redirect('game:day_four_task_one')  # change day_(num)_task_(next)
+#
